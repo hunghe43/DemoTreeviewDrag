2cb051eb325515dfbf9cc13da5bd146f8af0f118
hunghe43 <hungth11b@gmail.com>
2018-02-26 10:46:51 +0700

    Initial commit

---
+++ DemoTreeviewDrag/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace DemoTreeviewDrag
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TV1 = new System.Windows.Forms.TreeView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.treeViewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeviewToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeViewToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.Cms = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.alterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.Cms.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TV1
+            // 
+            this.TV1.AllowDrop = true;
+            this.TV1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TV1.Location = new System.Drawing.Point(0, 27);
+            this.TV1.Name = "TV1";
+            this.TV1.Size = new System.Drawing.Size(293, 245);
+            this.TV1.TabIndex = 2;
+            this.TV1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.NodeClick);
+            this.TV1.DragDrop += new System.Windows.Forms.DragEventHandler(this.Drop);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.treeViewToolStripMenuItem,
+            this.addToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(349, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // treeViewToolStripMenuItem
+            // 
+            this.treeViewToolStripMenuItem.Name = "treeViewToolStripMenuItem";
+            this.treeViewToolStripMenuItem.Size = new System.Drawing.Size(12, 20);
+            // 
+            // addToolStripMenuItem1
+            // 
+            this.addToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.treeviewToolStripMenuItem1,
+            this.treeViewToolStripMenuItem2});
+            this.addToolStripMenuItem1.Name = "addToolStripMenuItem1";
+            this.addToolStripMenuItem1.Size = new System.Drawing.Size(41, 20);
+            this.addToolStripMenuItem1.Text = "Add";
+            // 
+            // treeviewToolStripMenuItem1
+            // 
+            this.treeviewToolStripMenuItem1.Name = "treeviewToolStripMenuItem1";
+            this.treeviewToolStripMenuItem1.Size = new System.Drawing.Size(129, 22);
+            this.treeviewToolStripMenuItem1.Text = "1-treeview";
+            this.treeviewToolStripMenuItem1.Click += new System.EventHandler(this.treeviewToolStripMenuItem1_Click);
+            // 
+            // treeViewToolStripMenuItem2
+            // 
+            this.treeViewToolStripMenuItem2.Name = "treeViewToolStripMenuItem2";
+            this.treeViewToolStripMenuItem2.Size = new System.Drawing.Size(129, 22);
+            // 
+            // Cms
+            // 
+            this.Cms.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem,
+            this.deleteToolStripMenuItem,
+            this.alterToolStripMenuItem});
+            this.Cms.Name = "Cms";
+            this.Cms.Size = new System.Drawing.Size(108, 70);
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.addToolStripMenuItem.Text = "Add";
+            this.addToolStripMenuItem.Click += new System.EventHandler(this.addToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // alterToolStripMenuItem
+            // 
+            this.alterToolStripMenuItem.Name = "alterToolStripMenuItem";
+            this.alterToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.alterToolStripMenuItem.Text = "Alter";
+            this.alterToolStripMenuItem.Click += new System.EventHandler(this.alterToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 344);
+            this.Controls.Add(this.TV1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.Cms.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView TV1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem treeViewToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip Cms;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem alterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem treeviewToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem treeViewToolStripMenuItem2;
+    }
+}
+
---
+++ DemoTreeviewDrag/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.VisualBasic;
+
+namespace DemoTreeviewDrag
+{
+    public partial class Form1 : Form
+    {
+        TreeView tree;
+        TreeNode nodeKeo;
+        TreeNode node;
+        TreeNode nodeTha;
+        Point point;
+        public Form1()
+        {
+            InitializeComponent();
+            TV1.DragDrop += new DragEventHandler(Drop);
+
+            TV1.DragOver += new DragEventHandler(Over);
+
+            TV1.MouseDown += new MouseEventHandler(Down);
+
+            //TV1.NodeMouseClick += new TreeNodeMouseClickEventHandler(NodeClick);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var node1 = TV1.Nodes.Add("node 1");
+            var node2 = TV1.Nodes.Add("node 2");
+
+            node1.Nodes.Add("child node A1");
+            node1.Nodes.Add("child node A2");
+            node1.Nodes.Add("child node C1");
+
+
+            node2.Nodes.Add("child node D1");
+            node2.Nodes.Add("child node E1");
+            node2.Nodes.Add("child node F1");
+            node2.Nodes.Add("child node G1");
+
+            node1.Expand();
+            node2.Expand();
+        }
+        private void Down(object sender, MouseEventArgs e)
+        {
+            tree = (TreeView)sender;
+            nodeKeo = tree.GetNodeAt(e.X, e.Y);
+            tree.SelectedNode = nodeKeo;
+            if (nodeKeo != null)
+            {
+                tree.DoDragDrop(nodeKeo.Clone(), DragDropEffects.Copy);
+            }
+        }
+        private void Over(object sender, DragEventArgs e)
+        {
+            tree = (TreeView)sender;
+            if (e.Data.GetData(typeof(TreeNode)) != null)
+            {
+                point = new Point(e.X, e.Y);
+                point = tree.PointToClient(point);
+                nodeTha = tree.GetNodeAt(point);
+                if (nodeTha != null)
+                {
+                    e.Effect = DragDropEffects.Copy;
+                    tree.SelectedNode = nodeTha;
+                }
+            }
+        }
+        private void Drop(object sender, DragEventArgs e)
+        {
+            tree = (TreeView)sender;
+            if (nodeTha.Level == nodeKeo.Level-1)
+            {
+                if (nodeTha != null && !nodeTha.FullPath.Equals(nodeKeo.FullPath))
+                {
+                    TreeNode nodeTemp = nodeKeo;
+                    tree.Nodes.Remove(nodeKeo);
+                    nodeTha.Nodes.Add(nodeTemp);
+                    nodeTha.Expand();
+                }
+                else
+                {
+                    MessageBox.Show("Exitst node!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("not same Lever!");
+            }
+        }
+
+        private void NodeClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            node = ((TreeView)sender).SelectedNode;
+            point = new Point(MousePosition.X, MousePosition.Y);
+
+            Cms.Show(point);
+        }
+
+        private void treeviewToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            TV1.Nodes.Add(Interaction.InputBox("digiter o nome do node: ", "Criano node...", "SkoNode..."));
+        }
+
+
+        private void addToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            node.Nodes.Add(Interaction.InputBox("digiter o nome do children: ", "Criano children...", "SkoChildren..."));
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            node.Remove();
+        }
+
+        private void alterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            node.Text = Interaction.InputBox("digite o nome do node: ", "Alter node...", "Text");
+        }
+
+
+    }
+}
